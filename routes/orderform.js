8df4d7a879d5ff2783b826491aa5770f4b8c57f7
Dylan Dietz <dylan@bloomfieldhomes.net>
2024-11-04 17:13:56 -0600

    lint

--- routes/orderform.js
+++ routes/orderform.js
@@ -1,51 +1,51 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
 /* GET users listing. */
-router.get('/', async (_req, res, next) => {
+router.get("/", async (_req, res, next) => {
   try {
     const packages = await prisma.package.findMany({
       where: {
-        category: { equals: 'Package' }
-      }
+        category: { equals: "Package" },
+      },
     });
     const refrigerators = await prisma.package.findMany({
       where: {
-        category: { equals: 'Refrigerator' }
-      }
+        category: { equals: "Refrigerator" },
+      },
     });
     const w_ds = await prisma.package.findMany({
       where: {
-        category: { equals: 'Washer/Dryer' }
-      }
+        category: { equals: "Washer/Dryer" },
+      },
     });
 
-    res.render('orderform', {
+    res.render("orderform", {
       title: "Order Form",
       packages,
       refrigerators,
-      w_ds
+      w_ds,
     });
   } catch (error) {
     next(error);
   }
 });
 
-router.get('/packageItems', async (req, res, next) => {
+router.get("/packageItems", async (req, res, next) => {
   try {
     const packageId = parseInt(req.query.packageId);
     const packageItems = await prisma.packageItem.findMany({
       where: {
-        package_id: { equals: packageId }
+        package_id: { equals: packageId },
       },
       include: {
         package: true,
-        item: true
+        item: true,
       },
-      orderBy: { priority: 'asc' }
+      orderBy: { priority: "asc" },
     });
     res.json({ packageItems });
   } catch (error) {
@@ -54,4 +54,3 @@ router.get('/packageItems', async (req, res, next) => {
 });
 
 export default router;
-
