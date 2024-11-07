0cf6263c5dd01946bf73f5bb7af788831af11047
Dylan Dietz <dylan@bloomfieldhomes.net>
2024-11-07 16:42:44 -0600

    packages routes

--- routes/packages.js
+++ routes/packages.js
@@ -8,6 +8,22 @@ router.get("/", async (req, res) => {
   res.render("packages/index", await getPackages(req));
 });
 
+router.get("/edit", async (req, res) => {
+  const items = await prisma.item.findMany();
+  const itemCategories = getItemCategories(items);
+
+  const packages = await prisma.package.findMany();
+  const packageCategories = getPackageCategories(packages);
+
+  res.render("packages/form", {
+    title: "Maintain Packages",
+    items,
+    itemCategories,
+    packages,
+    packageCategories,
+  });
+});
+
 router.get("/packageItems", async (req, res, next) => {
   try {
     const packageId = parseInt(req.query.packageId);
@@ -26,5 +42,33 @@ router.get("/packageItems", async (req, res, next) => {
     next(error);
   }
 });
+
+const getItemCategories = (items) => {
+  if (!items) return [];
+
+  return Array.from(
+    new Set(
+      items
+        .map((item) => item.category.toLowerCase())
+        .filter((category) => category.trim() !== "")
+    )
+  )
+    .map((category) => category.charAt(0).toUpperCase() + category.slice(1))
+    .sort();
+};
+
+const getPackageCategories = (packages) => {
+  if (!packages) return [];
+
+  return Array.from(
+    new Set(
+      packages
+        .map((pkg) => pkg.category.toLowerCase())
+        .filter((category) => category.trim() !== "")
+    )
+  )
+    .map((category) => category.charAt(0).toUpperCase() + category.slice(1))
+    .sort();
+};
 
 export default router;
