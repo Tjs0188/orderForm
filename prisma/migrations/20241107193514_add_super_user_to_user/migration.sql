dc8a10fafd80b10d8f0579dfc38ae6cff2047c85
Dylan Dietz <dylan@bloomfieldhomes.net>
2024-11-07 13:38:37 -0600

    update database to unclude superAdmin on user

---
+++ prisma/migrations/20241107193514_add_super_user_to_user/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+    "superAdmin" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_User" ("admin", "createdAt", "email", "id", "name", "password", "role") SELECT "admin", "createdAt", "email", "id", "name", "password", "role" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
