dad6284c3395180a2710911c6e531dc3d1ea8192
dylan dietz <dylan817@live.com>
2024-11-08 15:12:25 -0600

    add lots of routes and error handling

--- routes/packages.js
+++ routes/packages.js
@@ -1,27 +1,74 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import multer from "multer";
 
 const router = express.Router();
 const prisma = new PrismaClient();
+const upload = multer(); // Initialize multer
 
-router.get("/", async (req, res) => {
-  res.render("packages/index", await getPackages(req));
+router.post("/", upload.none(), async (req, res) => {
+  try {
+    const { name, meta, category } = req.body;
+    const pkg = await prisma.package.create({
+      data: {
+        name,
+        meta,
+        category,
+      },
+    });
+
+    res.json(pkg);
+  } catch (error) {
+    console.log(error);
+    if (error.code === "P2002") {
+      res.status(400).send({ message: "Package already exists" });
+    } else {
+      res.status(400).send({ message: "An error occurred. Please try again." });
+    }
+  }
+});
+
+router.post("/items", upload.none(), async (req, res) => {
+  try {
+    const { product_number, description, category } = req.body;
+    const item = await prisma.item.create({
+      data: {
+        product_number,
+        description,
+        category,
+      },
+    });
+
+    res.json(item);
+  } catch (error) {
+    console.log(error);
+    if (error.code === "P2002") {
+      res.status(400).send({ message: "Item already exists" });
+    } else {
+      res.status(400).send({ message: "An error occurred. Please try again." });
+    }
+  }
 });
 
 router.get("/edit", async (req, res) => {
-  const items = await prisma.item.findMany();
-  const itemCategories = getItemCategories(items);
+  try {
+    const items = await prisma.item.findMany();
+    const itemCategories = getItemCategories(items);
 
-  const packages = await prisma.package.findMany();
-  const packageCategories = getPackageCategories(packages);
+    const packages = await prisma.package.findMany();
+    const packageCategories = getPackageCategories(packages);
 
-  res.render("packages/form", {
-    title: "Maintain Packages",
-    items,
-    itemCategories,
-    packages,
-    packageCategories,
-  });
+    res.render("packages/form", {
+      title: "Maintain Packages",
+      items,
+      itemCategories,
+      packages,
+      packageCategories,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: "An error occurred. Please try again." });
+  }
 });
 
 router.get("/packageItems", async (req, res, next) => {
@@ -37,9 +84,75 @@ router.get("/packageItems", async (req, res, next) => {
       },
       orderBy: { priority: "asc" },
     });
+
     res.json({ packageItems });
   } catch (error) {
     next(error);
+  }
+});
+
+router.put("/packageItems/:id", async (req, res, next) => {
+  try {
+    const packageItemId = parseInt(req.params.id);
+    const priority = parseInt(req.body.priority);
+
+    const packageItem = await prisma.packageItem.update({
+      where: {
+        id: packageItemId,
+      },
+      data: {
+        priority,
+      },
+    });
+
+    res.json(packageItem);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.post("/addItem", async (req, res, next) => {
+  const packageId = parseInt(req.body.package_id);
+  const itemId = parseInt(req.body.item_id);
+  const priority = req.body.priority;
+
+  try {
+    const highestPriority = await prisma.packageItem.aggregate({
+      where: {
+        package_id: { equals: packageId },
+      },
+      _max: {
+        priority: true,
+      },
+    });
+    const nextPriority = highestPriority._max.priority + 1;
+    const packageItem = await prisma.packageItem.create({
+      data: {
+        package_id: packageId,
+        item_id: itemId,
+        priority: parseInt(priority || nextPriority),
+      },
+      include: {
+        package: {
+          include: {
+            items: true,
+          },
+        },
+        item: true,
+      },
+    });
+
+    res.json(packageItem);
+  } catch (error) {
+    console.log(error);
+    if (error.code === "P2002") {
+      res.status(400).send({ message: "Item already exists in package" });
+    } else if (!itemId) {
+      res.status(400).send({ message: "Item is required" });
+    } else if (!packageId) {
+      res.status(400).send({ message: "Package is required" });
+    }
   }
 });
 
