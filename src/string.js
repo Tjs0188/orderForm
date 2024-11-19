b1a796334c1ed505d7e77982cb2f71fc4d3e1db1
Dylan Dietz <dylan@bloomfieldhomes.net>
2024-11-19 10:24:43 -0600

    remove unused code

--- src/string.js
+++ src/string.js
@@ -1,7 +1,5 @@
 function truncateString(str, num) {
   if (str.length > num) {
-    console.log("Truncating string...");
-    console.log("Original string: ", str);
     return str.slice(0, num) + "...";
   } else {
     return str;
@@ -12,16 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const elements = document.querySelectorAll(".truncate");
   elements.forEach((element) => {
     const truncateLength = element.dataset["truncatelength"] || 30;
-
     element.innerHTML = truncateString(element.innerHTML, truncateLength);
-  });
-
-  const tooltips = document.querySelectorAll(".hs-tooltip-content");
-  tooltips.forEach((tooltip) => {
-    const truncateLength = tooltip.dataset["truncatelength"] || 30;
-    tooltip.innerHTML = tooltip.innerHTML
-      .split("<br>")
-      .map((line) => truncateString(line, truncateLength))
-      .join("<br>");
   });
 });
