a588e5baf9320810a9be4181922d184518aa56ee
Dylan Dietz <dylan@bloomfieldhomes.net>
2024-11-19 10:24:18 -0600

    set default rows per page and format order data

--- routes/orderHistory.js
+++ routes/orderHistory.js
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 /* GET order histories listings. */
 router.get("/", async (req, res, next) => {
   const currentPage = parseInt(req.query.page) || 1;
-  const rowsPerPage = 10;
+  const rowsPerPage = 20;
   const skip = (currentPage - 1) * rowsPerPage;
   try {
     const [orders, totalCount] = await prisma.$transaction([
@@ -15,13 +15,37 @@ router.get("/", async (req, res, next) => {
         skip,
         take: rowsPerPage,
         where: { userId: req.currentUser.id },
+        orderBy: { createdAt: "desc" },
       }),
       prisma.orderHistory.count({ where: { userId: req.currentUser.id } }),
     ]);
 
+    const formatOrderData = (data) => {
+      const obj = JSON.parse(data);
+      return Object.entries(obj)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join("\n");
+    };
+
+    const formattedOrders = orders.map((order) => {
+      const formattedOrder = {
+        ...order,
+        formattedOrderData: formatOrderData(order.orderData).replace(
+          /\n/g,
+          "<br>"
+        ),
+        createdAt: order.createdAt.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }),
+      };
+      return formattedOrder;
+    });
+
     res.render("orderHistory", {
       title: "History",
-      orders,
+      orders: formattedOrders,
       totalCount,
       currentPage,
     });
