3c1072f42d98d6c7a24b95668072e54766ec5334
dylan dietz <dylan817@live.com>
2024-11-22 18:07:46 -0600

    add classes after modal success

--- src/templateModal.js
+++ src/templateModal.js
@@ -35,26 +35,42 @@ document.addEventListener("DOMContentLoaded", () => {
     try {
       // Send the form data using Axios
       const response = await axios.post("/orderform/saveTemplate", formData);
-
+      const successClasses = [
+        "border-green-400",
+        "text-green-400",
+        "animate-pulse",
+      ];
       HSOverlay.close(modalElement);
 
-      const templateSelect = document.querySelector("#template-select");
+      const templateSelect = document.getElementById("template-select");
+
       const option = document.createElement("option");
       option.value = response.data.template.id;
       option.text = response.data.template.name;
+      templateSelect.classList.add(...successClasses);
       templateSelect.appendChild(option);
-      templateSelect.querySelector(".no-templates").remove();
+      const noTemplates = templateSelect.querySelector(".no-templates");
+      if (noTemplates) {
+        noTemplates.remove();
+      }
+
+      setTimeout(() => {
+        templateSelect.classList.remove(...successClasses);
+      }, 5000);
+
       // Optionally, you can handle the response and update the UI accordingly
     } catch (error) {
+      console.log(error);
       const nameInput = templateForm.querySelector("#name-input");
-      const errorElement = document.createElement("p");
-
+      const errorElement = document.querySelector("#template_name_error");
       nameInput.classList.add("border-rose-500");
-
+      errorElement.classList.remove("hidden");
       errorElement.innerText = error.response.data.error;
       errorElement.classList.add("text-sm", "text-rose-500");
-
-      nameInput.insertAdjacentElement("afterend", errorElement);
+      setTimeout(() => {
+        nameInput.classList.remove("border-rose-500");
+        errorElement.classList.add("hidden");
+      }, 4000);
       // Optionally, you can handle the error and update the UI accordingly
     }
   });
